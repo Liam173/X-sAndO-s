646d70f2d7967a73451a2312af23143ab2b3d319
Liam173 <liamdupreez17@gmail.com>
2020-10-15 14:54:10 +0200

    Fixed the file format

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Project_2._0__X_s_and_O_s_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(226, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(440, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(12, 209);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(226, 209);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox5.TabIndex = 13;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(440, 209);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(12, 408);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox7.TabIndex = 15;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(226, 408);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox8.TabIndex = 16;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(440, 408);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(184, 159);
+            this.pictureBox9.TabIndex = 17;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 579);
+            this.splitter1.TabIndex = 18;
+            this.splitter1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(202, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(18, 555);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(416, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(18, 555);
+            this.richTextBox2.TabIndex = 20;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(9, 177);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(194, 26);
+            this.richTextBox3.TabIndex = 21;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(9, 376);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(194, 26);
+            this.richTextBox4.TabIndex = 22;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(216, 177);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(205, 26);
+            this.richTextBox5.TabIndex = 23;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(216, 376);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(205, 26);
+            this.richTextBox6.TabIndex = 24;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(430, 177);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(194, 26);
+            this.richTextBox7.TabIndex = 25;
+            this.richTextBox7.Text = "";
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.Location = new System.Drawing.Point(430, 376);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.Size = new System.Drawing.Size(194, 26);
+            this.richTextBox8.TabIndex = 26;
+            this.richTextBox8.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 579);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,337 @@
+﻿using Project_2._0__X_s_and_O_s_.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_2._0__X_s_and_O_s_
+{
+    public partial class Form1 : Form
+    {
+        Image o = Resources.O;
+        Image x = Resources.X;
+        Image def = Resources._default;
+        bool yeyOrNey = true;
+
+        int boxOne = 0;
+        int boxTwo = 0;
+        int boxThree = 0;
+        int boxFour = 0;
+        int boxFive = 0;
+        int boxSix = 0;
+        int boxSeven = 0;
+        int boxEight = 0;
+        int boxNine = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            setDefault();
+            setTBEnabledOff();
+            setBoxEnabledOn();
+        }
+
+        private void result()
+        {
+            if ((boxOne == 1) & (boxTwo == 1) & (boxThree == 1))
+            {
+                //Print player one won the game
+                MessageBox.Show("Player one won the game");
+                setBoxEnabledOff();
+            } else if ((boxOne == 2) & (boxTwo == 2) & (boxThree == 2))
+            {
+                //Print player two won the game
+                MessageBox.Show("Player two won the game");
+                setBoxEnabledOff();
+            } else if ((boxFour == 1) & (boxFive == 1) & (boxSix == 1))
+            {
+                //Print player one won the game
+                MessageBox.Show("Player one won the game");
+                setBoxEnabledOff();
+            } else if ((boxFour == 2) & (boxFive == 2) & (boxSix == 2))
+            {
+                //Print player two won the game
+                MessageBox.Show("Player two won the game");
+                setBoxEnabledOff();
+            } else if ((boxSeven == 1) & (boxEight == 1) & (boxNine == 1))
+            {
+                //Print player one won the game
+                MessageBox.Show("Player one won the game");
+                setBoxEnabledOff();
+            } else if ((boxSeven == 2) & (boxEight == 2) & (boxNine == 2))
+            {
+                //Print player two won the game
+                MessageBox.Show("Player two won the game");
+                setBoxEnabledOff();
+            } else if ((boxOne == 1) & (boxFour == 1) & (boxSeven == 1))
+            {
+                //Print player one won the game
+                MessageBox.Show("Player one won the game");
+                setBoxEnabledOff();
+            } else if ((boxOne == 2) & (boxFour == 2) & (boxSeven == 2))
+            {
+                //Print player two won the game
+                MessageBox.Show("Player two won the game");
+                setBoxEnabledOff();
+            } else if ((boxTwo == 1) & (boxFive == 1) & (boxEight == 1))
+            {
+                //Print player one won the game
+                MessageBox.Show("Player one won the game");
+                setBoxEnabledOff();
+            } else if ((boxTwo == 2) & (boxFive == 2) & (boxEight == 2))
+            {
+                //Print player two won the game
+                MessageBox.Show("Player two won the game");
+                setBoxEnabledOff();
+            } else if ((boxThree == 1)&(boxSix == 1)&(boxNine == 1))
+            {
+                //Print player one won the game
+                MessageBox.Show("Player one won the game");
+                setBoxEnabledOff();
+            } else if ((boxThree == 2) & (boxSix == 2) & (boxNine == 2))
+            {
+                //Print player two won the game
+                MessageBox.Show("Player two won the game");
+                setBoxEnabledOff();
+            } else if ((boxOne == 1) & (boxFive == 1) & (boxNine == 1))
+            {
+                //Print player one won the game
+                MessageBox.Show("Player one won the game");
+                setBoxEnabledOff();
+            } else if ((boxOne == 2) & (boxFive == 2) & (boxNine == 2))
+            {
+                //Print player two won the game
+                MessageBox.Show("Player two won the game");
+                setBoxEnabledOff();
+            } else if ((boxThree == 1)&(boxFive == 1)&(boxSeven == 1))
+            {
+                //Print player one won the game
+                MessageBox.Show("Player one won the game");
+                setBoxEnabledOff();
+            } else if ((boxThree == 2) &(boxFive == 2) & (boxSeven == 2))
+            {
+                //Print player two won the game
+                MessageBox.Show("Player two won the game");
+                setBoxEnabledOff();
+            }
+        }
+
+        private void setDefault()
+        {
+            pictureBox1.Image = def;
+            pictureBox2.Image = def;
+            pictureBox3.Image = def;
+            pictureBox4.Image = def;
+            pictureBox5.Image = def;
+            pictureBox6.Image = def;
+            pictureBox7.Image = def;
+            pictureBox8.Image = def;
+            pictureBox9.Image = def;
+        }
+
+        private void setBoxEnabledOn()
+        {
+            pictureBox1.Enabled = true;
+            pictureBox2.Enabled = true;
+            pictureBox3.Enabled = true;
+            pictureBox4.Enabled = true;
+            pictureBox5.Enabled = true;
+            pictureBox6.Enabled = true;
+            pictureBox7.Enabled = true;
+            pictureBox8.Enabled = true;
+            pictureBox9.Enabled = true;
+        }
+
+        private void setBoxEnabledOff()
+        {
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
+            pictureBox3.Enabled = false;
+            pictureBox4.Enabled = false;
+            pictureBox5.Enabled = false;
+            pictureBox6.Enabled = false;
+            pictureBox7.Enabled = false;
+            pictureBox8.Enabled = false;
+            pictureBox9.Enabled = false;
+        }
+
+        private void setTBEnabledOff()
+        {
+            richTextBox1.Enabled = false;
+            richTextBox2.Enabled = false;
+            richTextBox3.Enabled = false;
+            richTextBox4.Enabled = false;
+            richTextBox5.Enabled = false;
+            richTextBox6.Enabled = false;
+            richTextBox7.Enabled = false;
+            richTextBox8.Enabled = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox1.Image = o;
+                yeyOrNey = false;
+                boxOne = 1;
+            }
+            else
+            {
+                pictureBox1.Image = x;
+                yeyOrNey = true;
+                boxOne = 2;
+            }
+            result();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox2.Image = o;
+                yeyOrNey = false;
+                boxTwo = 1;
+            }
+            else
+            {
+                pictureBox2.Image = x;
+                yeyOrNey = true;
+                boxTwo = 2;
+            }
+            result();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox3.Image = o;
+                yeyOrNey = false;
+                boxThree = 1;
+            }
+            else
+            {
+                pictureBox3.Image = x;
+                yeyOrNey = true;
+                boxThree = 2;
+            }
+            result();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox4.Image = o;
+                yeyOrNey = false;
+                boxFour = 1;
+            }
+            else
+            {
+                pictureBox4.Image = x;
+                yeyOrNey = true;
+                boxFour = 2;
+            }
+            result();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox5.Image = o;
+                yeyOrNey = false;
+                boxFive = 1;
+            }
+            else
+            {
+                pictureBox5.Image = x;
+                yeyOrNey = true;
+                boxFive = 2;
+            }
+            result();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            pictureBox6.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox6.Image = o;
+                yeyOrNey = false;
+                boxSix = 1;
+            }
+            else
+            {
+                pictureBox6.Image = x;
+                yeyOrNey = true;
+                boxSix = 2;
+            }
+            result();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            pictureBox7.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox7.Image = o;
+                yeyOrNey = false;
+                boxSeven = 1;
+            }
+            else
+            {
+                pictureBox7.Image = x;
+                yeyOrNey = true;
+                boxSeven = 2;
+            }
+            result();
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            pictureBox8.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox8.Image = o;
+                yeyOrNey = false;
+                boxEight = 1;
+            }
+            else
+            {
+                pictureBox8.Image = x;
+                yeyOrNey = true;
+                boxEight = 2;
+            }
+            result();
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            pictureBox9.Enabled = false;
+            if (yeyOrNey == true)
+            {
+                pictureBox9.Image = o;
+                yeyOrNey = false;
+                boxNine = 1;
+            }
+            else
+            {
+                pictureBox9.Image = x;
+                yeyOrNey = true;
+                boxNine = 2;
+            }
+            result();
+        }
+    }
+}
